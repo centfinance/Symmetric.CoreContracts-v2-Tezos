6d6d48ee85ea75b4c28bb48a026b1a15864cc2d0
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-02-02 15:47:05 +0000

    Set types and add error message to FA12 core

---
+++ contracts/pool-utils/pool-token/SymmetricPoolToken.py
@@ -0,0 +1,8 @@
+import smartpy as sp
+
+import contracts.utils.tokens.FA12 as FA12
+
+
+class SymmetricPoolToken(FA12):
+    def __init__(self):
+        pass
--- contracts/utils/tokens/FA12.py
+++ contracts/utils/tokens/FA12.py
@@ -4,37 +4,70 @@
 import smartpy as sp
 
 
+class FA12_Error:
+    def make(s):
+        return "FA1.2_" + s
+
+    NotAdmin = make("NotAdmin")
+    NotEnoughBalance = make("NotEnoughBalance")
+    UnsafeAllowanceChange = make("UnsafeAllowanceChange")
+    NotEnoughAllowance = make("NotEnoughAllowance")
+    MaxSupplyMinted = make("MaxSupplyMinted")
+    TezSentToEntrypoint = make("TezSentToEntrypoint")
+
+
 class FA12_core(sp.Contract):
     def __init__(self, **extra_storage):
-        self.init(balances=sp.big_map(tvalue=sp.TRecord(approvals=sp.TMap(
-            sp.TAddress, sp.TNat), balance=sp.TNat)), totalSupply=0, **extra_storage)
+        self.init(
+            balances=sp.big_map(
+                tvalue=sp.TRecord(approvals=sp.TMap(
+                    sp.TAddress, sp.TNat), balance=sp.TNat),
+            ),
+            totalSupply=0,
+            **extra_storage,
+        )
 
     @sp.entry_point
     def transfer(self, params):
-        sp.set_type(params, sp.TRecord(from_=sp.TAddress, to_=sp.TAddress,
-                    value=sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
-        sp.verify(self.is_administrator(sp.sender) |
-                  (~self.is_paused() &
-                   ((params.from_ == sp.sender) |
-                      (self.data.balances[params.from_].approvals[sp.sender] >= params.value))))
+        sp.set_type(
+            params,
+            sp.TRecord(from_=sp.TAddress, to_=sp.TAddress, value=sp.TNat).layout(
+                ("from_ as from", ("to_ as to", "value"))
+            ),
+        )
+        sp.verify(
+            (params.from_ == sp.sender)
+            | (self.data.balances[params.from_].approvals.get(sp.sender, 0) >= params.value),
+            FA12_Error.NotEnoughAllowance,
+        )
+
+        # Reject tez
+        sp.verify(sp.amount == sp.tez(0), FA12_Error.TezSentToEntrypoint)
+
+        self.addAddressIfNecessary(params.from_)
         self.addAddressIfNecessary(params.to_)
-        sp.verify(self.data.balances[params.from_].balance >= params.value)
+        sp.verify(self.data.balances[params.from_].balance >=
+                  params.value, FA12_Error.NotEnoughBalance)
         self.data.balances[params.from_].balance = sp.as_nat(
             self.data.balances[params.from_].balance - params.value)
         self.data.balances[params.to_].balance += params.value
-        with sp.if_(params.from_ != sp.sender) & (~self.is_administrator(sp.sender)):
+
+        with sp.if_(params.from_ != sp.sender):
             self.data.balances[params.from_].approvals[sp.sender] = sp.as_nat(
-                self.data.balances[params.from_].approvals[sp.sender] - params.value)
+                self.data.balances[params.from_].approvals[sp.sender] -
+                params.value
+            )
 
     @sp.entry_point
     def approve(self, params):
+        sp.verify(sp.amount == sp.tez(0), FA12_Error.TezSentToEntrypoint)
         sp.set_type(params, sp.TRecord(spender=sp.TAddress,
                     value=sp.TNat).layout(("spender", "value")))
-        sp.verify(~self.is_paused())
+        self.addAddressIfNecessary(sp.sender)
         alreadyApproved = self.data.balances[sp.sender].approvals.get(
             params.spender, 0)
-        sp.verify((alreadyApproved == 0) | (
-            params.value == 0), "UnsafeAllowanceChange")
+        sp.verify((alreadyApproved == 0) | (params.value == 0),
+                  FA12_Error.UnsafeAllowanceChange)
         self.data.balances[sp.sender].approvals[params.spender] = params.value
 
     def addAddressIfNecessary(self, address):
@@ -43,6 +76,7 @@ class FA12_core(sp.Contract):
 
     @sp.onchain_view()
     def getBalance(self, params):
+        sp.set_type(params, sp.TAddress)
         with sp.if_(self.data.balances.contains(params)):
             sp.result(self.data.balances[params].balance)
         with sp.else_():
@@ -50,11 +84,15 @@ class FA12_core(sp.Contract):
 
     @sp.onchain_view()
     def getAllowance(self, params):
-        sp.result(self.data.balances[params.owner].approvals[params.spender])
+        sp.set_type(params, sp.TRecord(owner=sp.TAddress, spender=sp.TAddress))
+        with sp.if_(self.data.balances.contains(params.owner)):
+            sp.result(self.data.balances[params.owner].approvals.get(
+                params.spender, 0))
+        with sp.else_():
+            sp.result(sp.nat(0))
 
     @sp.onchain_view()
-    def getTotalSupply(self, params):
-        sp.set_type(params, sp.TUnit)
+    def getTotalSupply(self):
         sp.result(self.data.totalSupply)
 
     # this is not part of the standard but can be supported through inheritance.
