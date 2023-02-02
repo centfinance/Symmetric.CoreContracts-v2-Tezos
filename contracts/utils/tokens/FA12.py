da17220b7a0a4cd53017061ab84388e60209f63a
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-02-02 14:56:33 +0000

    Add FA1.2 core contract

---
+++ contracts/utils/tokens/FA12.py
@@ -0,0 +1,66 @@
+# Fungible Assets - FA12
+# Inspired by https://gitlab.com/tzip/tzip/blob/master/A/FA1.2.md
+
+import smartpy as sp
+
+
+class FA12_core(sp.Contract):
+    def __init__(self, **extra_storage):
+        self.init(balances=sp.big_map(tvalue=sp.TRecord(approvals=sp.TMap(
+            sp.TAddress, sp.TNat), balance=sp.TNat)), totalSupply=0, **extra_storage)
+
+    @sp.entry_point
+    def transfer(self, params):
+        sp.set_type(params, sp.TRecord(from_=sp.TAddress, to_=sp.TAddress,
+                    value=sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
+        sp.verify(self.is_administrator(sp.sender) |
+                  (~self.is_paused() &
+                   ((params.from_ == sp.sender) |
+                      (self.data.balances[params.from_].approvals[sp.sender] >= params.value))))
+        self.addAddressIfNecessary(params.to_)
+        sp.verify(self.data.balances[params.from_].balance >= params.value)
+        self.data.balances[params.from_].balance = sp.as_nat(
+            self.data.balances[params.from_].balance - params.value)
+        self.data.balances[params.to_].balance += params.value
+        with sp.if_(params.from_ != sp.sender) & (~self.is_administrator(sp.sender)):
+            self.data.balances[params.from_].approvals[sp.sender] = sp.as_nat(
+                self.data.balances[params.from_].approvals[sp.sender] - params.value)
+
+    @sp.entry_point
+    def approve(self, params):
+        sp.set_type(params, sp.TRecord(spender=sp.TAddress,
+                    value=sp.TNat).layout(("spender", "value")))
+        sp.verify(~self.is_paused())
+        alreadyApproved = self.data.balances[sp.sender].approvals.get(
+            params.spender, 0)
+        sp.verify((alreadyApproved == 0) | (
+            params.value == 0), "UnsafeAllowanceChange")
+        self.data.balances[sp.sender].approvals[params.spender] = params.value
+
+    def addAddressIfNecessary(self, address):
+        with sp.if_(~ self.data.balances.contains(address)):
+            self.data.balances[address] = sp.record(balance=0, approvals={})
+
+    @sp.utils.view(sp.TNat)
+    def getBalance(self, params):
+        with sp.if_(self.data.balances.contains(params)):
+            sp.result(self.data.balances[params].balance)
+        with sp.else_():
+            sp.result(sp.nat(0))
+
+    @sp.utils.view(sp.TNat)
+    def getAllowance(self, params):
+        sp.result(self.data.balances[params.owner].approvals[params.spender])
+
+    @sp.utils.view(sp.TNat)
+    def getTotalSupply(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.totalSupply)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_paused(self):
+        return sp.bool(False)
+
+    # this is not part of the standard but can be supported through inheritance.
+    def is_administrator(self, sender):
+        return sp.bool(False)
