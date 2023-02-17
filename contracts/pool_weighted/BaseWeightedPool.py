5060b91dc09ac0ac2a58f924971a852e25fbf1c0
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-02-17 12:09:09 +0000

    Add _onInitializePool to BaseWeightedPool

--- contracts/pool_weighted/BaseWeightedPool.py
+++ contracts/pool_weighted/BaseWeightedPool.py
@@ -4,6 +4,8 @@ import contracts.interfaces.SymmetricErrors as Errors
 
 import contracts.utils.helpers.ScalingHelpers as ScalingHelpers
 
+import contracts.pool_weighted.WeightedMath as WeightedMath
+
 from contracts.pool_utils.BasePool import BasePool
 
 
@@ -32,18 +34,22 @@ class BaseWeightedPool(
 
         amountsIn = params.userData.amountsIn
 
-        sp.verify(sp.len(amountsIn.length) == sp.len(params.scalingFactors))
-        ScalingHelpers._upscaleArray(amountsIn, params.scalingFactors)
+        length = sp.len(amountsIn)
+        sp.verify(length == sp.len(params.scalingFactors))
+
+        upscaledAmounts = ScalingHelpers._upscaleArray(
+            amountsIn, params.scalingFactors)
 
         invariantAfterJoin = WeightedMath._calculateInvariant(
-            self.data.normalizedWeights, amountsIn)
+            self.data.normalizedWeights, upscaledAmounts)
 
-        # // Set the initial BPT to the value of the invariant times the number of tokens. This makes BPT supply more
-        # // consistent in Pools with similar compositions but different number of tokens.
-        sptAmountOut = Math.mul(invariantAfterJoin, amountsIn.length)
+        # Set the initial SPT to the value of the invariant times the number of tokens. This makes SPT supply more
+        # consistent in Pools with similar compositions but different number of tokens.
+        # sptAmountOut = Math.mul(invariantAfterJoin, amountsIn.length)
+        sptAmountOut = invariantAfterJoin * length
 
-        # // Initialization is still a join, so we need to do post-join work. Since we are not paying protocol fees,
-        # // and all we need to do is update the invariant, call `_updatePostJoinExit` here instead of `_afterJoinExit`.
+        # Initialization is still a join, so we need to do post-join work. Since we are not paying protocol fees,
+        # and all we need to do is update the invariant, call `_updatePostJoinExit` here instead of `_afterJoinExit`.
         self._updatePostJoinExit(invariantAfterJoin)
 
         return (sptAmountOut, amountsIn)
