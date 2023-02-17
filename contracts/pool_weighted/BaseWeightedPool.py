c905ebbb4757786863a6fb9260db5277cb2bc6ae
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-02-17 11:59:01 +0000

    Fix ScalingHelpers to use Map instead of List

--- contracts/pool_weighted/BaseWeightedPool.py
+++ contracts/pool_weighted/BaseWeightedPool.py
@@ -1,5 +1,9 @@
 import smartpy as sp
 
+import contracts.interfaces.SymmetricErrors as Errors
+
+import contracts.utils.helpers.ScalingHelpers as ScalingHelpers
+
 from contracts.pool_utils.BasePool import BasePool
 
 
@@ -20,3 +24,26 @@ class BaseWeightedPool(
             symbol,
             owner,
         )
+
+    def _onInitializePool(self, params):
+        kind = params.userData.kind
+        # TODO: Use an enum
+        sp.verify(kind == 'INIT', Errors.UNINITIALIZED)
+
+        amountsIn = params.userData.amountsIn
+
+        sp.verify(sp.len(amountsIn.length) == sp.len(params.scalingFactors))
+        ScalingHelpers._upscaleArray(amountsIn, params.scalingFactors)
+
+        invariantAfterJoin = WeightedMath._calculateInvariant(
+            self.data.normalizedWeights, amountsIn)
+
+        # // Set the initial BPT to the value of the invariant times the number of tokens. This makes BPT supply more
+        # // consistent in Pools with similar compositions but different number of tokens.
+        sptAmountOut = Math.mul(invariantAfterJoin, amountsIn.length)
+
+        # // Initialization is still a join, so we need to do post-join work. Since we are not paying protocol fees,
+        # // and all we need to do is update the invariant, call `_updatePostJoinExit` here instead of `_afterJoinExit`.
+        self._updatePostJoinExit(invariantAfterJoin)
+
+        return (sptAmountOut, amountsIn)
