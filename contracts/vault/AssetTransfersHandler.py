b47bcc6d0dd5d85db468c7d659aebe8de92fe3bf
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-03-01 12:38:58 +0000

    Add TransferTokens function to AssetTransfersHandler

---
+++ contracts/vault/AssetTransfersHandler.py
@@ -0,0 +1,94 @@
+import smartpy as sp
+
+
+class AssetTransfersHandler:
+
+    def TransferFATwoTokens(sender, receiver, amount, tokenAddress, id):
+        """Transfers FA2 tokens
+
+        Args:
+            sender: sender address
+            receiver: receiver address
+            amount: amount of tokens to be transferred
+            tokenAddress: address of the FA2 contract
+            id: id of token to be transferred
+        """
+
+        arg = [
+            sp.record(
+                from_=sender,
+                txs=[
+                    sp.record(
+                        to_=receiver,
+                        token_id=id,
+                        amount=amount
+                    )
+                ]
+            )
+        ]
+
+        transferHandle = sp.contract(
+            sp.TList(sp.TRecord(from_=sp.TAddress, txs=sp.TList(sp.TRecord(
+                amount=sp.TNat, to_=sp.TAddress, token_id=sp.TNat).layout(("to_", ("token_id", "amount")))))),
+            tokenAddress,
+            entry_point='transfer').open_some()
+
+        sp.transfer(arg, sp.mutez(0), transferHandle)
+
+    def TransferFATokens(sender, reciever, amount, tokenAddress):
+        """Transfers FA1.2 tokens
+
+        Args:
+            sender: sender address
+            reciever: reciever address
+            amount: amount of tokens to be transferred
+            tokenAddress: address of the FA1.2 contract
+        """
+
+        TransferParam = sp.record(
+            from_=sender,
+            to_=reciever,
+            value=amount
+        )
+
+        transferHandle = sp.contract(
+            sp.TRecord(from_=sp.TAddress, to_=sp.TAddress, value=sp.TNat).layout(
+                ("from_ as from", ("to_ as to", "value"))),
+            tokenAddress,
+            "transfer"
+        ).open_some()
+
+        sp.transfer(TransferParam, sp.mutez(0), transferHandle)
+
+    def TransferToken(sender, receiver, amount, tokenAddress, id, faTwoFlag):
+        """Generic function to transfer any type of tokens
+
+        Args:
+            sender: sender address
+            receiver: receiver address
+            amount: amount of tokens to be transferred
+            tokenAddress: address of the token contract
+            id: id of token to be transferred (for FA2 tokens)
+            faTwoFlag: boolean describing whether the token contract is FA2 or not
+        """
+
+        sp.verify(amount > 0, "Zero_Transfer")
+
+        with sp.if_(faTwoFlag):
+
+            AssetTransfersHandler.TransferFATwoTokens(
+                sender, receiver, amount, tokenAddress, id)
+
+        with sp.else_():
+
+            AssetTransfersHandler.TransferFATokens(
+                sender, receiver, amount, tokenAddress)
+
+    def _receiveAsset(asset, amount, sender, fromInternalBalance):
+        pass
+
+    def _sendAsset(asset, amount, recipient, toInternalBalance):
+        pass
+
+    def _handleRemainingXtz(amountUsed):
+        pass
