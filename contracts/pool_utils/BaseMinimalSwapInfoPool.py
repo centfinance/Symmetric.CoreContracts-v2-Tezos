2e2c788b40c522ca021c7f09bfb78cbfedcc42f9
stxphxn <991266+stxphxn@users.noreply.github.com>
2023-03-03 11:52:54 +0000

    Create BaseMinimalSwapInfoPool.onSwap entry point 💱

---
+++ contracts/pool_utils/BaseMinimalSwapInfoPool.py
@@ -0,0 +1,64 @@
+import smartpy as sp
+
+import contracts.utils.helpers.ScalingHelpers as ScalingHelpers
+
+
+class BaseMinimalSwapInfoPool:
+
+    def onSwap(
+        self,
+        request,
+        balanceTokenIn,
+        balanceTokenOut
+    ):
+        # TODO: Check it's not paused
+
+        scalingFactorTokenIn = self._scalingFactor(request.tokenIn)
+        scalingFactorTokenOut = self._scalingFactor(request.tokenOut)
+
+        balanceTokenIn = ScalingHelpers._upscale(
+            balanceTokenIn, scalingFactorTokenIn)
+        balanceTokenOut = ScalingHelpers._upscale(
+            balanceTokenOut, scalingFactorTokenOut)
+
+        swapAmount = sp.compute(0)
+        with sp.if_(request.kind == ''):
+            swapAmount = self._subtractSwapFeeAmount(request.amount)
+
+            swapAmount = ScalingHelpers._upscale(
+                swapAmount, scalingFactorTokenIn)
+
+            swapRequest = sp.record(
+                tokenIn=request.tokenIn,
+                tokenOut=request.tokenOut,
+                amount=swapAmount,
+            )
+
+            amountOut = self._onSwapGivenIn(sp.record(
+                swapRequest=swapRequest,
+                currentBalanceTokenIn=balanceTokenIn,
+                currentBalanceTokenOut=balanceTokenOut,
+            ))
+
+            swapAmount = ScalingHelpers._downscaleDown(
+                amountOut, scalingFactorTokenOut)
+        with sp.else_():
+            swapAmount = ScalingHelpers._upscale(
+                request.amount, scalingFactorTokenOut)
+
+            swapRequest = sp.record(
+                tokenIn=request.tokenIn,
+                tokenOut=request.tokenOut,
+                amount=swapAmount,
+            )
+
+            amountIn = self._onSwapGivenOut(sp.record(
+                swapRequest=swapRequest,
+                currentBalanceTokenIn=balanceTokenIn,
+                currentBalanceTokenOut=balanceTokenOut,
+            ))
+
+            downscaleAmount = ScalingHelpers._downscaleUp(
+                amountIn, scalingFactorTokenIn)
+
+            swapAmount = self._addSwapFeeAmount(downscaleAmount)
